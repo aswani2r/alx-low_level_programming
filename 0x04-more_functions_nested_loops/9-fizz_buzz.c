363ed97e7e69338212a96473d8c77a54ad87c1b5
aswani2r <ruudaswani26@gmail.com>
2023-06-23 20:04:01 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - prints the number from 1 to 100, followed by new line
  * but for multiples of three prints fizz instead
  * and for multiples of five prints buzz instead
  * Return always: 0 (success)
  */
-
 void fizzBuzz() {
     int i;
     for (i = 1; i <= 100; i++) {
@@ -22,7 +20,6 @@ void fizzBuzz() {
     }
     printf("\n");
 }
-
 int main() {
     fizzBuzz();
     return 0;
