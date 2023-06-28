b805079cb19daf1d3bf775d8d6855063ffd859c2
aswani2r <ruudaswani26@gmail.com>
2023-06-28 16:46:39 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,14 @@
  * return: void
  */
 
-void print_most_numbers(void) {
-    int i;
+void print_most_numbers(void)
+{
+	char c;
 
-    for (i = 0; i <= 9; i++) {
-        if (i != 2 && i != 4) {
-            _putchar(i + '0');
-        }
-    }
-    _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
+	_putchar('\n');
 }
