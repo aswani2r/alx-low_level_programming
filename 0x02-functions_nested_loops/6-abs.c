b428fad014f4f5563817b538eeddceb2fa7ff62b
aswani2r <ruudaswani26@gmail.com>
2023-06-28 15:04:48 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
-/**
-* _abs - Computes the absolute value of an integer
-* @n: The number to be computed
-* Return: Absolute value of number or zero
-*/
-int int_abs(int c) {
-int abs_val = c * -1;
-return abs_val;
-}
-int main() {
-int num = -5;
-int result = int_abs(num);
-printf("The absolute value of %d is %d\n", num, result);
-return 0;
-}
 
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_last_digit  - prints the last digit of  a number
  * @n: The number to be treated
  * Return: Value of the last digit of number
  */
-int print_last_digit(int n)
-{
-int last = n % 10;
-if (last < 0)
-{
-last= last * -1;
+int print_last_digit(int number) {
+int last_digit = number % 10;
+std::cout << "Last digit: " << last_digit << std::endl;
+return last_digit;
 }
-putchar(last + '0');
-return last;
+int main() {
+int number = 12345;
+int last_digit = print_last_digit(number);
+std::cout << "Returned last digit: " << last_digit << std::endl;
+return 0;
 }
