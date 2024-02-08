afe43394989f9a61da3877cc34cb28b003c2a395
aswani2r <ruudaswani26@gmail.com>
2024-02-07 23:58:49 -0800

    Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,25 +11,31 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t low = 0, high = size - 1;
+    size_t low =  0, high = size -  1;
+    int index = -1;
 
     if (!array || !size)
         return (-1);
 
     while (low <= high && value >= array[low] && value <= array[high])
     {
-       size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+        size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
         printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
         if (array[pos] == value)
-		return (-1);
-
-        if (array[pos] < value)
-            low = pos + 1;
-	else
-		high = pos - 1;
+        {
+            index = pos;
+            break;
+        }
+        else if (array[pos] < value)
+            low = pos +  1;
+        else
+            high = pos -  1;
     }
-    printf("Value [%d] is not present in the array\n", value);
-    return (-1);
+    
+    if (index == -1)
+        printf("Value [%d] is not present in the array\n", value);
+
+    return (index);
 }
